65eca421921b8bd9734d31fa2c9342ae50961a20
AnAbakumova <miss.abackumowa@yandex.ru>
2023-06-13 15:28:52 +0300

    branch eraser added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,10 @@ public class Main {
         people.add(new Person("Erick", "Maerson", 30));
         people.add(new Person("Kit", "Tshelokov", 27));
         people.add(new Person("Ann", "Brain Jane Kravs", 25));
-        Collections.sort(people, new PersonComparator(2));
+        people.add(new Person("Lein", "Sky", 16));
+        people.add(new Person("James", "Brucks", 17));
+
+        people.removeIf((person) -> person.getAge() < 18);
         System.out.println(people);
     }
 }
